270cec0389aa8696442f939716d3a5feac20e993
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-12-06 20:57:10 -0300

    first commit 2

---
+++ XamarinApi/XamarinApi/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using XamarinApi.Views;
+using Xamarin.Forms;
+
+namespace XamarinApi
+{
+    public partial class MainPage : Shell
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(CountrysPage), typeof(CountrysPage));
+        }
+
+        
+    }
+}
---
+++ XamarinApi/XamarinApi/Views/Principal.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinApi.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Principal : ContentPage
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        async void OnCountryClicked(object Sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(nameof(CountrysPage));
+        }
+    }
+}
