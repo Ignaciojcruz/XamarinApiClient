2ab83b6c677164666089081ece81ae7ccd7c85f6
Ignacio Cruz <ignaciojcruz@hotmail.com>
2022-12-07 19:08:34 -0300

    api conectada

---
+++ XamarinApi/XamarinApi/Data/RestServiceCountry.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System; 
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinApi.Models;
+
+namespace XamarinApi.Data
+{
+    public class RestServiceCountry
+    {
+        string BaseUrl;
+        string modelUrl;
+
+        public RestServiceCountry()
+        {
+            BaseUrl = Constants.webapiUrl;
+            modelUrl = "api/Country";
+        }
+
+        public async Task<List<Country>> GetCountrysAsync()
+        {
+            List<Country> countryList = new List<Country>();
+
+            try
+            {
+                using(var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(this.BaseUrl);
+                    client.DefaultRequestHeaders.Clear();
+
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    HttpResponseMessage response = await client.GetAsync(modelUrl);
+
+                    if(response.IsSuccessStatusCode)
+                    {
+                        var countryResponse = response.Content.ReadAsStringAsync().Result;
+                        countryList = JsonConvert.DeserializeObject<List<Country>>(countryResponse);
+                    }
+
+                }
+            }catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            
+            return countryList;
+        }
+    }
+}
--- XamarinApi/XamarinApi/Views/CountrysPage.xaml.cs
+++ XamarinApi/XamarinApi/Views/CountrysPage.xaml.cs
@@ -1,20 +1,30 @@
-﻿using System;
+﻿using XamarinApi.Data;
+using XamarinApi.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System;
 
 namespace XamarinApi.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CountrysPage : ContentPage
+    public partial class CountrysPage : ContentPage        
     {
+
+        RestServiceCountry _restService;
+
         public CountrysPage()
         {
             InitializeComponent();
+            _restService = new RestServiceCountry();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            List<Country> list = await _restService.GetCountrysAsync();
+            collectionView.ItemsSource = list;
+
         }
     }
 }
